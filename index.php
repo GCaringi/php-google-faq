ce23d11612d122d22cf90c9bcfd58148f4ee28a4
GCaringi <giannicaringi94@gmail.com>
2022-06-23 13:27:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$faq = [
+    [
+        'title' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+        'content' => "<p>La recente <a href='#'>decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.</p>"
+    ]
+];
+
+?>
